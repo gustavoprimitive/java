13729e5f31e92dfe98d96ef5dc783058b988b0ac
Gustavo Tejerina Santos <39309543+gustavoprimitive@users.noreply.github.com>
2018-07-15 01:14:04 +0200

    Add files via upload

---
+++ Cuadrado Latino/src/com/sudoku/pruebaSudoku.java
@@ -0,0 +1,12 @@
+package com.sudoku;
+
+public class pruebaSudoku {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GeneraCuadricula sudoku = new GeneraCuadricula();
+		sudoku.recorridoCuadricula();
+		//sudoku.pintadoCuadricula(sudoku.llenarCuadricula());		
+	}
+
+}
