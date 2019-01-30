af5a5f79101399217ae92eb0f8418b10f063d862
Gustavo Tejerina <gustavotejerinasantos@gmail.com>
2019-01-30 23:37:30 +0100

    ObtenerPinFuerzaBruta

--- ObtenerPinFuerzaBruta/src/PinFuerzaBruta.java
+++ ObtenerPinFuerzaBruta/src/PinFuerzaBruta.java
@@ -1,9 +1,13 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class PinFuerzaBruta {
 
 	public final int[] enterosPosibles = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	public String pin;
+	
 	
 	/**
 	 * @param pin
@@ -30,27 +34,31 @@ public class PinFuerzaBruta {
 	 */
 	public String concatDigitos(ArrayList<Integer> digitos) {
 		String auxConcat = "";
+		
 		for (int num : digitos) {
 			auxConcat = auxConcat.concat(String.valueOf(num));
 		}
 		return auxConcat;
 	}
 	
+	
 	/**
+	 * @throws UnknownHostException 
 	 * 
 	 */
-	public void permute() {
+	public void permute() throws UnknownHostException {		
 		ArrayList<Integer> auxConcat = new ArrayList<Integer>();
+		
 		for (int n1 : enterosPosibles) {
 			for (int n2 : enterosPosibles) {
 				for (int n3 : enterosPosibles) {
 					for (int n4 : enterosPosibles) {
 						auxConcat.add(n1); auxConcat.add(n2); auxConcat.add(n3); auxConcat.add(n4);							
 						if (compruebaCoincidencia(concatDigitos(auxConcat))) {
-							System.out.println("Ok: " + n1 + "-" + n2 + "-" + n3 + "-" + n4);
+							System.out.println(loggerLine() + "Resuelto:\t" + n1 + "-" + n2 + "-" + n3 + "-" + n4);
 							return;
 						}else {
-							System.out.println(n1 + "-" + n2 + "-" + n3 + "-" + n4);
+							System.out.println(loggerLine() + "Intento:\t" + n1 + "-" + n2 + "-" + n3 + "-" + n4);
 							auxConcat.removeAll(auxConcat);
 						}							
 					}					
@@ -58,5 +66,30 @@ public class PinFuerzaBruta {
 			}			
 		}
 	}
+	
+	/**
+	 * @return
+	 * @throws UnknownHostException
+	 */
+	public String loggerHost() throws UnknownHostException {
+		return InetAddress.getLocalHost().getHostAddress();
+	}
+
+
+	/**
+	 * @return
+	 * @throws UnknownHostException
+	 */
+	public String loggerLine() throws UnknownHostException {
+		Date timestamp = new Date();
+		return (timestamp + "\t" + loggerHost() + "\t");
+	}
+	
 }
+
+
+
+
+
+
 
--- ObtenerPinFuerzaBruta/src/ejecutaPinFuerzaBruta.java
+++ ObtenerPinFuerzaBruta/src/ejecutaPinFuerzaBruta.java
@@ -1,19 +1,22 @@
+import java.net.UnknownHostException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class ejecutaPinFuerzaBruta {
 
-	static String pin = "";
+	static int numDigitos = 4;
+	static String pin = "";	
+	static String patron = "^[0-9]{" + numDigitos + "}$"; 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 		// TODO Auto-generated method stub		
 		
 		Scanner sc = new Scanner(System.in);
 		
-		while (!Pattern.matches("^[0-9]{4}$", pin)) {			
-			System.out.print("Código PIN (4 dígitos numéricos enteros): ");
+		while (!Pattern.matches(patron, pin)) {			
+			System.out.print("Código PIN (" + numDigitos + " dígitos numéricos enteros): ");
 			pin = sc.next();
-			Pattern.matches("^[0-9]{4}$", pin);
+			Pattern.matches(patron, pin);
 		}
 				
 		PinFuerzaBruta myPinFuerzaBruta = new PinFuerzaBruta(pin);
@@ -21,5 +24,5 @@ public class ejecutaPinFuerzaBruta {
 		
 		sc.close();
 		
-	}
+	}	
 }
