7ce51a63c80877d6800fa251db7a0dc5b8ddde33
Gustavo Tejerina <gustavotejerinasantos@gmail.com>
2019-02-03 19:58:15 +0100

    ObtenerPinFuerzaBruta

--- ObtenerPinFuerzaBruta/src/com/pin/EjecutaPinFuerzaBruta.java
+++ ObtenerPinFuerzaBruta/src/com/pin/EjecutaPinFuerzaBruta.java
@@ -9,23 +9,25 @@ public class EjecutaPinFuerzaBruta {
 	static int numDigitos = 4;
 	static String pin = "";	
 	static String patron = "^[0-9|a-z|A-Z]{" + numDigitos + "}$"; 
+	boolean exit = false;
 	
-	public static void main(String[] args) throws UnknownHostException {
-		// TODO Auto-generated method stub		
+	
+	public static void main(String[] args) throws UnknownHostException {		
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);  
 		
 		//Validación de código de entrada con expresión regular
 		while (!Pattern.matches(patron, pin)) {			
 			System.out.print("Código PIN (" + numDigitos + " dígitos alfanuméricos): ");
 			pin = sc.next();
 			Pattern.matches(patron, pin);
-		}
-		
+		}		
+        
 		//Instanciación de clase y llamada a método de permutación
 		PinFuerzaBruta myPinFuerzaBruta = new PinFuerzaBruta(pin);
-		myPinFuerzaBruta.permute();
+		myPinFuerzaBruta.execute();
 		
 		sc.close();		
-	}	
+	}
+		
 }
--- ObtenerPinFuerzaBruta/src/com/pin/PinFuerzaBruta.java
+++ ObtenerPinFuerzaBruta/src/com/pin/PinFuerzaBruta.java
@@ -4,12 +4,15 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class PinFuerzaBruta {
 
 	final char[] valoresPosibles = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	String pin;
+	String pin;	
 	ArrayList<Character> auxConcat = new ArrayList<Character>();
+
 	
 	/**
 	 * Constructor
@@ -25,6 +28,7 @@ public class PinFuerzaBruta {
 	 * @return
 	 */
 	private boolean compruebaCoincidencia(String candidato) {
+		
 		if (candidato.equals(pin)) {			
 			return true;
 		}else {
@@ -48,22 +52,50 @@ public class PinFuerzaBruta {
 	
 	
 	/**
+	 * Ejecución en paralelo de los métodos "permute" y "revPermute"
+	 */
+	public void execute(){
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        executorService.submit(() -> {
+			try {
+				permute(executorService);
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+        executorService.submit(() -> {
+			try {
+				revPermute(executorService);
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+
+        executorService.shutdown();
+    }
+
+	
+	/**
 	 * Permutación de valores
 	 * @throws UnknownHostException 
 	 * 
 	 */
-	public void permute() throws UnknownHostException {		
+	private void permute(ExecutorService exec) throws UnknownHostException {		
 
 		for (Character v1 : valoresPosibles) {
 			for (Character v2 : valoresPosibles) {
 				for (Character v3 : valoresPosibles) {
 					for (Character v4 : valoresPosibles) {
 						auxConcat.add(v1); auxConcat.add(v2); auxConcat.add(v3); auxConcat.add(v4);							
+						//System.out.println(loggerLine() + "Intento:\t" + v1 + "-" + v2 + "-" + v3 + "-" + v4);
 						if (compruebaCoincidencia(concatDigitos(auxConcat))) {
-							System.out.println(loggerLine() + "Resuelto:\t" + v1 + "-" + v2 + "-" + v3 + "-" + v4);
-							return;
-						}else {
-							System.out.println(loggerLine() + "Intento:\t" + v1 + "-" + v2 + "-" + v3 + "-" + v4);
+							System.out.println(loggerLine() + "Resuelto:\t" + v1 + "-" + v2 + "-" + v3 + "-" + v4 + " (desde hilo " + Thread.currentThread().getId() + ")");
+							exec.shutdown();
+							return;							
+						}else {							
 							auxConcat.removeAll(auxConcat);
 						}							
 					}					
@@ -71,6 +103,37 @@ public class PinFuerzaBruta {
 			}			
 		}
 	}
+	
+	
+	/**
+	 * Permutación de valores en orden inverso
+	 * @param exec
+	 * @throws UnknownHostException
+	 */
+	private void revPermute(ExecutorService exec) throws UnknownHostException {		
+
+		for (int i1=valoresPosibles.length-1; i1>=0; i1--) {
+			char v1 = valoresPosibles[i1];
+			for (int i2=valoresPosibles.length-1; i2>=0; i2--)  {
+			char v2 = valoresPosibles[i2];
+				for (int i3=valoresPosibles.length-1; i3>=0; i3--) {
+				char v3 = valoresPosibles[i3];
+					for (int i4=valoresPosibles.length-1; i4>=0; i4--) {
+					char v4 = valoresPosibles[i4];
+						auxConcat.add(v1); auxConcat.add(v2); auxConcat.add(v3); auxConcat.add(v4);							
+						//System.out.println(loggerLine() + "Intento:\t" + v1 + "-" + v2 + "-" + v3 + "-" + v4);
+						if (compruebaCoincidencia(concatDigitos(auxConcat))) {
+							System.out.println(loggerLine() + "Resuelto:\t" + v1 + "-" + v2 + "-" + v3 + "-" + v4 + " (desde hilo " + Thread.currentThread().getId() + ")");
+							exec.shutdown();
+							return;							
+						}else {							
+							auxConcat.removeAll(auxConcat);
+						}							
+					}					
+				}				
+			}			
+		}
+	}	
 	
 	/**
 	 * Nombre de host para traza de output
@@ -87,9 +150,9 @@ public class PinFuerzaBruta {
 	 * @return
 	 * @throws UnknownHostException
 	 */
-	private String loggerLine() throws UnknownHostException {
+	public String loggerLine() throws UnknownHostException {
 		Date timestamp = new Date();
-		return (timestamp + "\t" + loggerHost() + "\t");
+		return (timestamp + "\t" + loggerHost() + "\tThreads: " + Thread.activeCount() + "\t");
 	}
 	
 }
