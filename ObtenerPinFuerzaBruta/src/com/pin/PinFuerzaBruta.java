d6b0008f7b242129bc5ea2b4b30b2738e96f18ad
Gustavo Tejerina <gustavotejerinasantos@gmail.com>
2019-02-04 00:06:57 +0100

    ObtenerPinFuerzaBruta

--- ObtenerPinFuerzaBruta/src/com/pin/PinFuerzaBruta.java
+++ ObtenerPinFuerzaBruta/src/com/pin/PinFuerzaBruta.java
@@ -11,7 +11,10 @@ public class PinFuerzaBruta {
 
 	final char[] valoresPosibles = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 	String pin;	
-	ArrayList<Character> auxConcat = new ArrayList<Character>();
+	ArrayList<Character> auxConcatPermute = new ArrayList<Character>();
+	ArrayList<Character> auxConcatRevPermute = new ArrayList<Character>();
+	Date comienzoEjec;
+	boolean esfinEjec = false;
 
 	
 	/**
@@ -57,9 +60,10 @@ public class PinFuerzaBruta {
 	public void execute(){
         ExecutorService executorService = Executors.newFixedThreadPool(2);
 
+        comienzoEjec = new Date();
         executorService.submit(() -> {
 			try {
-				permute(executorService);
+				permute();
 			} catch (UnknownHostException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -67,7 +71,7 @@ public class PinFuerzaBruta {
 		});
         executorService.submit(() -> {
 			try {
-				revPermute(executorService);
+				revPermute();
 			} catch (UnknownHostException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -83,20 +87,21 @@ public class PinFuerzaBruta {
 	 * @throws UnknownHostException 
 	 * 
 	 */
-	private void permute(ExecutorService exec) throws UnknownHostException {		
+	private void permute() throws UnknownHostException {		
 
 		for (Character v1 : valoresPosibles) {
 			for (Character v2 : valoresPosibles) {
 				for (Character v3 : valoresPosibles) {
 					for (Character v4 : valoresPosibles) {
-						auxConcat.add(v1); auxConcat.add(v2); auxConcat.add(v3); auxConcat.add(v4);							
+						auxConcatPermute.add(v1); auxConcatPermute.add(v2); auxConcatPermute.add(v3); auxConcatPermute.add(v4);							
 						//System.out.println(loggerLine() + "Intento:\t" + v1 + "-" + v2 + "-" + v3 + "-" + v4);
-						if (compruebaCoincidencia(concatDigitos(auxConcat))) {
-							System.out.println(loggerLine() + "Resuelto:\t" + v1 + "-" + v2 + "-" + v3 + "-" + v4 + " (desde hilo " + Thread.currentThread().getId() + ")");
-							exec.shutdown();
-							return;							
+						if (compruebaCoincidencia(concatDigitos(auxConcatPermute)) || esfinEjec) {
+							if (!esfinEjec) {
+								finEjec(v1, v2, v3, v4);
+							}
+							return;
 						}else {							
-							auxConcat.removeAll(auxConcat);
+							auxConcatPermute.removeAll(auxConcatPermute);
 						}							
 					}					
 				}				
@@ -110,7 +115,7 @@ public class PinFuerzaBruta {
 	 * @param exec
 	 * @throws UnknownHostException
 	 */
-	private void revPermute(ExecutorService exec) throws UnknownHostException {		
+	private void revPermute() throws UnknownHostException {		
 
 		for (int i1=valoresPosibles.length-1; i1>=0; i1--) {
 			char v1 = valoresPosibles[i1];
@@ -120,20 +125,35 @@ public class PinFuerzaBruta {
 				char v3 = valoresPosibles[i3];
 					for (int i4=valoresPosibles.length-1; i4>=0; i4--) {
 					char v4 = valoresPosibles[i4];
-						auxConcat.add(v1); auxConcat.add(v2); auxConcat.add(v3); auxConcat.add(v4);							
+					auxConcatRevPermute.add(v1); auxConcatRevPermute.add(v2); auxConcatRevPermute.add(v3); auxConcatRevPermute.add(v4);							
 						//System.out.println(loggerLine() + "Intento:\t" + v1 + "-" + v2 + "-" + v3 + "-" + v4);
-						if (compruebaCoincidencia(concatDigitos(auxConcat))) {
-							System.out.println(loggerLine() + "Resuelto:\t" + v1 + "-" + v2 + "-" + v3 + "-" + v4 + " (desde hilo " + Thread.currentThread().getId() + ")");
-							exec.shutdown();
-							return;							
+						if (compruebaCoincidencia(concatDigitos(auxConcatRevPermute)) || esfinEjec) {
+							if (!esfinEjec) {
+								finEjec(v1, v2, v3, v4);
+							}
+							return;
 						}else {							
-							auxConcat.removeAll(auxConcat);
+							auxConcatRevPermute.removeAll(auxConcatRevPermute);
 						}							
 					}					
 				}				
 			}			
 		}
 	}	
+	
+	/**
+	 * Output de finde ejecución
+	 * @param v1
+	 * @param v2
+	 * @param v3
+	 * @param v4
+	 * @throws UnknownHostException
+	 */
+	private void finEjec(char v1, char v2, char v3, char v4) throws UnknownHostException {
+		System.out.println(loggerLine() + "Resuelto:\t" + v1 + "-" + v2 + "-" + v3 + "-" + v4 + " (desde hilo " + Thread.currentThread().getId() + ")");
+		loggerSegEjec();
+		esfinEjec = true;			
+	}
 	
 	/**
 	 * Nombre de host para traza de output
@@ -144,6 +164,15 @@ public class PinFuerzaBruta {
 		return InetAddress.getLocalHost().getHostAddress();
 	}
 
+	/**
+	 * Tiempo de ejecución
+	 */
+	private void loggerSegEjec() {
+		Date finEjec= new Date();
+		long diferenciaMs = Math.abs(finEjec.getTime() - comienzoEjec.getTime());
+		long diferenciaSs = diferenciaMs / 1000; 
+		System.out.println(diferenciaSs + " segundos " + (diferenciaMs - (diferenciaSs) * 1000) + " milisegundos");
+	}
 
 	/**
 	 * Fecha y hora para traza de output
